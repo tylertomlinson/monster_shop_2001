a987c5550b32b0d6582e8b63c63a91b1c77d1b4f
Alex Latham <alexlthm@gmail.com>
2020-04-07 15:50:06 -0600

    Refactor users#create to utilize @users.errors for flash messages, replace redirect with render

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,25 +6,22 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            session[:user_id] = @user.id    
-            flash[:notice] = "Welcome #{@user.name}! You are now registered and logged in!"
-            redirect_to "/profile"
+          session[:user_id] = @user.id
+          flash[:notice] = "Welcome #{@user.name}! You are now registered and logged in!"
+          redirect_to "/profile"
         else
-            flash[:notice] = "The email and/or password were incorrect, please try again." 
-            redirect_to "/register"
+          flash[:error] = @user.errors.full_messages.to_sentence
+          render :new
         end
     end
 
     def show
         @user = current_user
-    end 
-
+    end
 
     private
 
     def user_params
         params.permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
     end
-
-    
-end 
+end
