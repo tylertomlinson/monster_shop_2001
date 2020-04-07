35d41bc5b725f05427adee47ec3cce5a4845b39f
Alex Latham <alexlthm@gmail.com>
2020-04-07 15:50:50 -0600

    Add validations to user model for presence of required fields and unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-    validates_presence_of :name, :address, :city, :state, :zip, :password
+    validates_presence_of :name, :address, :city, :state, :zip, :email, :password
     validates_uniqueness_of :email
 end
