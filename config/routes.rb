9c73678b9c9d126b9ac3668afb5d8c19c0950903
Alex Latham <alexlthm@gmail.com>
2020-04-07 19:40:21 -0600

    Add admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -37,8 +37,6 @@ Rails.application.routes.draw do
   get "/register", to: "users#new"
   post "/register", to: "users#create"
 
-  get "/profile", to: "users#show"
-
   namespace :merchant do
     get "/", to: "dashboard#index"
   end
