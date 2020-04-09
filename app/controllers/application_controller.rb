8ff5fe77b8413560be226498f263347d011a59c8
Alex Latham <alexlthm@gmail.com>
2020-04-09 13:43:50 -0600

    Change visibility of certain nav bar links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart, :current_user, :current_merchant?, :current_admin?
+  helper_method :cart, :current_user, :current_regular?, :current_merchant?, :current_admin?
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
