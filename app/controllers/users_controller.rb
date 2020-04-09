853d4352f85bde75c8318faabd62b3ebff527ea6
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-04-08 18:09:50 -0600

    Add update + edit password to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,6 @@ class UsersController < ApplicationController
   def new
   end
 
-  def show
-    @user = current_user
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -33,6 +29,22 @@ class UsersController < ApplicationController
       flash[:error] = @user.errors.full_messages.to_sentence
       render :edit
     end
+  end
+
+  def update_password
+    user = current_user
+    user.update(user_params)
+    if user.save
+      flash[:notice] = "Your password has been updated."
+      redirect_to '/profile'
+    else
+      flash[:error] = user.errors.full_messages.to_sentence
+      redirect_to "/profile/edit_password"
+    end
+
+  end
+
+  def edit_password
   end
 
   private
