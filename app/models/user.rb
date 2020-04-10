d9487749ea25ce5c0e8149985a6cee13fee0f67c
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-04-10 17:46:54 -0600

    Update DB to have merchant_id on user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates_presence_of :name, :address, :city, :state, :zip, :email, :password
   validates_uniqueness_of :email
+  belongs_to :merchant, optional: true
 
   enum role: %w(regular merchant admin)
 end
