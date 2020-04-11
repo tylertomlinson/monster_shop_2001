813b188fccb69c7bbb28a36d283d91ab269670ee
Alex Latham <alexlthm@gmail.com>
2020-04-10 19:03:54 -0600

    Add migration for status in orders table, update model and spec

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order <ApplicationRecord
-  validates_presence_of :name, :address, :city, :state, :zip
+  validates_presence_of :name, :address, :city, :state, :zip, :user_id, :status
 
   belongs_to :user
   has_many :item_orders
