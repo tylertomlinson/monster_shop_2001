5c1866fc16399f7ec95b9791ac0f4e2d3e20ea15
Alex Latham <alexlthm@gmail.com>
2020-04-11 13:36:14 -0600

    Add order show page route

--- config/routes.rb
+++ config/routes.rb
@@ -63,5 +63,6 @@ Rails.application.routes.draw do
   namespace :profile do
     get "/", to: "dashboard#index"
     get "/orders", to: "dashboard#orders"
+    get "/orders/:id", to: "dashboard#order"
   end
 end
