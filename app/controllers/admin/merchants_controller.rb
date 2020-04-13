f622a059bfefd4989a3b9acd3a5c62df9fbc6778
Alex Latham <alexlthm@gmail.com>
2020-04-13 16:37:22 -0600

    Refactor admin/merchants#update action to bring coverage back to 100%

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -7,12 +7,11 @@ class Admin::MerchantsController < ApplicationController
 
   def update
     merchant = Merchant.find(params[:id])
-    merchant.toggle(:active?) if params[:active?] == "toggle"
-    if merchant.save
+    if params[:active?] == "toggle"
+      merchant.toggle(:active?)
+      merchant.save
       flash[:success] = "The account for #{merchant.name} has been #{merchant.active? ? "enabled" : "disabled"}"
       redirect_to admin_merchants_path
-    else
-      flash[:error] = merchant.errors.full_messages.to_sentence
     end
   end
 end
