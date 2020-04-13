17869fd77e33fccd394770fa45b8257435ecb64b
Mariana Cid <cidmariana5@gmail.com>
2020-04-13 13:15:35 -0600

    items_index_spec added to features

--- spec/features/merchant/items_index_spec.rb
+++ spec/features/merchant/items_index_spec.rb
@@ -4,6 +4,8 @@ describe "As a merchant employee", type: :feature do
   describe "When I visit  my merchant dashboard" do
     before(:each) do
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      employee = create(:merchant_employee, role: 1, merchant_id: @meg.id)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(employee)
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
       @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
@@ -12,7 +14,7 @@ describe "As a merchant employee", type: :feature do
     it 'I see a link to view my own items' do
       visit '/merchant'
 
-      click_link 'View Items'
+      click_link('View items')
 
       expect(current_path).to eq('/merchant/items') 
     end
