8c1915827b09e2db6035d4a26cbcfc692f1c40b6
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-04-13 18:29:18 -0600

    Added assocation test for US34

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe User, type: :model do
 
   describe "relationships" do
     it {should have_many :orders}
+    it {should belong_to(:merchant).optional}
   end
 
   describe "roles" do
