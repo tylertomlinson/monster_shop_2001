c4fe0c93bf6d0979e66e74e94461aacd8fadb427
Alex Latham <alexlthm@gmail.com>
2020-04-14 21:41:15 -0600

    Add test for fulfilling items

--- spec/features/merchant/orders/show_spec.rb
+++ spec/features/merchant/orders/show_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe "As a merchant employee" do
   before(:each) do
     @merchant = create(:merchant)
     user = create(:merchant_employee)
-    item1 = create(:item, inventory: 5, merchant: @merchant)
-    item2 = create(:item, inventory: 1, merchant: @merchant)
+    @item1 = create(:item, inventory: 5, merchant: @merchant)
+    @item2 = create(:item, inventory: 1, merchant: @merchant)
     @order = user.orders.create!(name: user.name, address: user.address, city: user.city, state: user.state, zip: user.zip)
-    @item_order1 = @order.item_orders.create!(item: item1, price: item1.price, quantity: 1)
-    @item_order2 = @order.item_orders.create!(item: item2, price: item2.price, quantity: 2)
+    @item_order1 = @order.item_orders.create!(item: @item1, price: @item1.price, quantity: 1)
+    @item_order2 = @order.item_orders.create!(item: @item2, price: @item2.price, quantity: 2)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit "/merchant/orders/#{@order.id}"
   end
@@ -35,6 +35,19 @@ RSpec.describe "As a merchant employee" do
   end
 
   it "I can fulfill part of an order" do
+    within("#item-#{@item_order1.item_id}") do
+      expect(page).to have_content("unfulfilled")
+      click_link("fulfill item")
+    end
 
+    expect(current_path).to eq("/merchant/orders/#{@order.id}")
+    expect(page).to have_content("You have fulfilled an order for 1 #{@item_order1.item.name}")
+
+    within("#item-#{@item_order1.item_id}") do
+      expect(page).to have_content("fulfilled")
+    end
+
+    @item1.reload
+    expect(@item1.inventory).to eq(4)
   end
 end
