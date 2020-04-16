2111d2ded8364b921e5cba850e46458e674ad09f
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-04-15 22:10:20 -0600

    Add show and index to admin orders controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,17 @@
 class Admin::OrdersController < ApplicationController
   before_action :require_admin
 
-    def update
-        @order = Order.find(params[:id])
-        @order.ship
-        redirect_to admin_path
-    end
+  def update
+    @order = Order.find(params[:id])
+    @order.ship
+    redirect_to admin_path
+  end
+
+  def index
+    @user = User.find(params[:user_id])
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
 end
