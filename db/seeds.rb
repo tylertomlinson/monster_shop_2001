28e83b36c10277da0e13580b40dbd73b215a14e1
Alex Latham <alexlthm@gmail.com>
2020-04-15 19:27:54 -0600

    Update image for pull toy

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,11 +17,11 @@ Item.destroy_all
 tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
 
 #dog_shop items
-pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "https://cdn.shopify.com/s/files/1/1114/2810/products/Plan-Toys-Dancing-Alligator-Pull-Along-Toy_1080x.jpg?v=1539482341", inventory: 32)
 dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
 
 User.destroy_all
 #users
 User.create(name: "Regular User", address: "1234 Regular St", city: "Regular City", state: "Regular State", zip: "00000", email: "user@example.com", password: "password_regular", role: 0)
-User.create(name: "Merchant Employee", address: "1234 Merchant St", city: "Merchant City", state: "Merchant State", zip: "00001", email: "merchant@example.com", password: "password_merchant", merchant_id: bike_shop.id, role: 1)
+User.create(name: "Merchant Employee", address: "1234 Merchant St", city: "Merchant City", state: "Merchant State", zip: "00001", email: "merchant@example.com", password: "password_merchant", merchant_id: dog_shop.id, role: 1)
 User.create(name: "Admin User", address: "1234 Admin St", city: "Admin City", state: "Admin State", zip: "00002", email: "admin@example.com", password: "password_admin", role: 2)
