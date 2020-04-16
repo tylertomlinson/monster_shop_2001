46d9ea1c20d5c2800785e50a6abee07e331e379e
Mariana Cid <cidmariana5@gmail.com>
2020-04-15 20:38:22 -0600

    flash notice created

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -35,6 +35,7 @@ class CartController < ApplicationController
   def increment_decrement
     if params[:increment_decrement] == "increment"
       cart.add_item(params[:item_id]) unless cart.limit_reached?(params[:item_id])
+      flash[:notice] = "Cart Quantity has reached maximum capacity."
     elsif params[:increment_decrement] == "decrement"
       cart.remove_item(params[:item_id])
       return remove_item if cart.quantity_zero?(params[:item_id])
