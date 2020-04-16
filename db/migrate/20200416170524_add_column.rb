ae6ed7cb5154f88eb1293ef96dc57f69c9a36325
Alex Latham <alexlthm@gmail.com>
2020-04-16 17:46:37 -0600

    Remove redundant migration

--- db/migrate/20200416170524_add_column.rb
+++
@@ -1,6 +0,0 @@
-class AddColumn < ActiveRecord::Migration[5.1]
-  def change
-    add_column :orders, :status, :integer, default:1
-    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
-  end
-end
